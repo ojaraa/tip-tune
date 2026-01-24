aa37d919803a2e6b394dac0a2ad382383593564e
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2026-01-24 13:08:28 +0100

    feat(frontend): add /badges route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
+import BadgesPage from './pages/BadgesPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/badges" element={<BadgesPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
